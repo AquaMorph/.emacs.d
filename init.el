1f9a3c8f9a0b785b42cce4fb2218d58f41ac7fb6
Christian Colglazier <ccolglazier@lenovo.com>
2018-01-31 13:11:01 -0500

    Added new default font, line numbers, and window size

--- init.el
+++ init.el
@@ -3,14 +3,14 @@
 (load "~/.emacs.d/packages")
 
 (custom-set-variables
-	'(ansi-color-faces-vector
-		[default default default italic underline success warning error])
-	'(ansi-color-names-vector
-		["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
-	'(custom-enabled-themes (quote (manoj-dark)))
-	'(package-selected-packages (quote (olivetti fountain-mode cyberpunk-theme)))
-	'(fountain-mode-hook (quote (turn-on-olivetti-mode imenu-list-minor-mode flyspell-mode)))
-	)
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(ansi-color-names-vector
+   ["black" "red3" "ForestGreen" "yellow3" "blue" "magenta3" "DeepSkyBlue" "gray50"])
+ '(custom-enabled-themes (quote (manoj-dark)))
+ '(package-selected-packages (quote (olivetti fountain-mode cyberpunk-theme)))
+ '(fountain-mode-hook (quote (turn-on-olivetti-mode imenu-list-minor-mode flyspell-mode)))
+ )
 
 (add-hook 'org-mode-hook (lambda () (flyspell-mode) (visual-line-mode t)))
 
@@ -18,7 +18,15 @@
 (menu-bar-mode 1)
 (toggle-scroll-bar -1) 
 (tool-bar-mode -1)
-(setq inhibit-startup-screen t) 
+(setq inhibit-startup-screen t)
+(add-to-list 'default-frame-alist '(font . "inconsolata-10"))
+
+; Enable Line Numbers
+(global-linum-mode t)
+
+; Startup Window size
+(add-to-list 'default-frame-alist '(width  . 120))
+(add-to-list 'default-frame-alist '(height . 40))
 
 ; Spellcheck
 (add-to-list 'exec-path "C:/Program Files (x86)/Aspell/bin/")
