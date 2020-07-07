1be7913e1497820ced53f79a22ca5eae931e7202
Christian Colglazier <christian@cacolglazier.com>
2020-07-07 13:15:57 -0400

    Removed go autocomplete

--- packages.el
+++ packages.el
@@ -5,7 +5,7 @@
 
 ; list the packages you want
 (setq package-list
-	'(fountain-mode imenu-list olivetti cyberpunk-theme flyspell ispell focus rainbow-mode auto-complete jedi markdown-mode vlf go-mode go-autocomplete))
+	'(fountain-mode imenu-list olivetti cyberpunk-theme flyspell ispell focus rainbow-mode auto-complete jedi markdown-mode vlf go-mode))
 
 ; activate all the packages
 (package-initialize)
